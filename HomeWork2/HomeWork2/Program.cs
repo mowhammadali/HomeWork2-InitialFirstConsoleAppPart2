3707aca4bd65f48924d144f13e0374d6fee42495
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-06-11 19:55:50 +0330

    [implement] task 4

--- HomeWork2/HomeWork2/Program.cs
+++ HomeWork2/HomeWork2/Program.cs
@@ -1,10 +1,28 @@
 ﻿using Task1;
 using Task2;
+using Task3;
 
 class Program
 {
     static void Main(string[] args)
     {
-        
+        // Task 3
+        Statistics statistics = new Statistics();
+
+        Console.Write("Enter input1: ");
+        string input1 = Console.ReadLine();
+        Console.Write("Enter input2: ");
+        string input2 = Console.ReadLine();
+
+        if (int.TryParse(input1, out int a) && (int.TryParse(input2, out int b)))
+        {
+            int c;
+
+            statistics.AnalyzeNumbers(a, ref b, out c);
+
+            Console.WriteLine($"a: {a}");
+            Console.WriteLine($"b: {b}");
+            Console.WriteLine($"c: {c}");
+        }
     }
 }
---
+++ HomeWork2/HomeWork2/Task3/Task3.cs
@@ -0,0 +1,14 @@
+﻿namespace Task3;
+
+class Statistics
+{
+    public void AnalyzeNumbers(int x, ref int y, out int z)
+    {
+        int sum = x + y;
+        z = sum;
+
+        y = y * y;
+
+        Console.WriteLine($"First parameter: {x}");
+    }
+}
